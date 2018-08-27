141cbdabbb1a66740d6ff7dece436bbdf7a63a7e
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-27 00:48:20 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,5 @@
-# require modules here
-
+require 'yaml'
+require 'pry'
 def load_library(emoticon_file)
    emoticons = YAML.load_file('./lib/emoticons.yml')
    emoticon_lib = {'get_meaning'  => {},
