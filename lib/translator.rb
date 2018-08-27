66164e909b6e8cec16b553e1e6de0273115b17d7
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-27 01:02:56 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,11 +23,11 @@ def get_japanese_emoticon(path, emoticon)
   result
 end
 
-def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
-  	  library = load_library(file_path)
-  if library["get_meaning"].include?(japanese_emoticon) 
-    library["get_meaning"][japanese_emoticon]
-  else
-    "Sorry, that emoticon was not found"
+def get_english_meaning((path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_meaning"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
   end
+  result
 end
